0d754f3d98fa010c7821785fe9fedb0eb269e39f
kmeaw <kmeaw@kmeaw.com>
2021-10-20 02:39:29 +0300

    Implement RCON protocol

--- client.go
+++ client.go
@@ -15,7 +15,7 @@ import (
 	"golang.org/x/net/context/ctxhttp"
 )
 
-const HELIX_BASE = "https://api.twitch.tv/helix/channel_points/custom_rewards"
+const HELIX_REWARDS = "https://api.twitch.tv/helix/channel_points/custom_rewards"
 const APP_ID = "ehx3943o3ttw0nplenassv0pme8yvb"
 const APP_SCOPES = "channel:manage:redemptions"
 
@@ -101,12 +101,28 @@ func (c *TwitchClient) apiCall(ctx context.Context, api_url string, payload map[
 	if payload == nil || len(payload) == 0 {
 		req, err = http.NewRequest("GET", api_url, nil)
 	} else {
+		method := "POST"
+
+		if mi, ok := payload["_method"]; ok {
+			if ms, ok := mi.(string); ok {
+				m := make(map[string]interface{})
+				for k := range payload {
+					if k == "_method" {
+						continue
+					}
+					m[k] = payload[k]
+				}
+				payload = m
+				method = ms
+			}
+		}
+
 		data, err = json.Marshal(payload)
 		if err != nil {
 			return fmt.Errorf("cannot marshal payload: %w", err)
 		}
 
-		req, err = http.NewRequest("POST", api_url, bytes.NewBuffer(data))
+		req, err = http.NewRequest(method, api_url, bytes.NewBuffer(data))
 	}
 
 	if err != nil {
@@ -117,6 +133,7 @@ func (c *TwitchClient) apiCall(ctx context.Context, api_url string, payload map[
 		req.Header.Set("Authorization", "Bearer "+c.Token)
 	}
 	req.Header.Set("User-Agent", "github.com/kmeaw/zdrst")
+	req.Header.Set("Client-ID", APP_ID)
 
 	resp, err = ctxhttp.Do(ctx, c.h, req)
 	if err != nil {
@@ -169,6 +186,52 @@ type twitchValidateReply struct {
 	ExpiresIn      int64    `json:"expires_in"`
 }
 
+type twitchRewardsReply struct {
+	Data []Reward `json:"data"`
+}
+
+type RewardImages struct {
+	Url1x string `json:"url_1x"`
+	Url2x string `json:"url_2x"`
+	Url4x string `json:"url_4x"`
+}
+
+type Reward struct {
+	ID string `json:"id"`
+
+	BroadcasterID int64 `json:"broadcaster_id,string"`
+	BroadcasterLogin string `json:"broadcaster_login"`
+	BroadcasterName string `json:"broadcaster_name"`
+
+	Title string `json:"title"`
+	Prompt string `json:"prompt"`
+	Cost int `json:"cost"`
+	Image *RewardImages `json:"image"`
+	DefaultImage *RewardImages `json:"image"`
+	BackgroundColor string `json:"background_color"`
+	IsEnabled bool `json:"is_enabled"`
+	IsUserInputRequired bool `json:"is_user_input_required"`
+
+	MaxPerStreamSetting struct {
+		IsEnabled bool `json:"is_enabled"`
+		MaxPerStream int `json:"max_per_stream"`
+	} `json:"max_per_stream_setting"`
+	MaxPerUserPerStreamSetting struct {
+		IsEnabled bool `json:"is_enabled"`
+		MaxPerUserPerStream int `json:"max_per_user_per_stream"`
+	} `json:"max_per_user_per_stream_setting"`
+	GlobalCooldownSetting struct {
+		IsEnabled bool `json:"is_enabled"`
+		GlobalCooldownSeconds int `json:"global_cooldown_seconds"`
+	} `json:"max_per_user_per_stream_setting"`
+
+	IsPaused bool `json:"is_paused"`
+	IsInStock bool `json:"is_in_stock"`
+	ShouldRedemptionsSkipRequestQueue bool `json:"should_redemptions_skip_request_queue"`
+	RedemptionsRedeemedCurrentStream *int `json:"redemptions_redeemed_current_stream"`
+	CooldownExpiresAt *time.Time `json:"cooldown_expires_at"`
+}
+
 func (c *TwitchClient) Prepare(ctx context.Context) error {
 	var v twitchValidateReply
 
@@ -205,6 +268,18 @@ func (c *TwitchClient) Prepare(ctx context.Context) error {
 	c.ExpiresAt = time.Now().Add(time.Second * time.Duration(v.ExpiresIn))
 
 	return nil
+}
+
+func (c *TwitchClient) Rewards(ctx context.Context) (rs []Reward, err error) {
+	var v twitchRewardsReply
+	err = c.apiCall(
+		ctx,
+		"https://api.twitch.tv/helix/channel_points/custom_rewards",
+		nil,
+		&v,
+	)
+
+	return v.Data, err
 }
 
 // vim: ai:ts=8:sw=8:noet:syntax=go
--- rcon.go
+++ rcon.go
@@ -1,13 +1,58 @@
 package main
 
 import (
+	"log"
 	"net"
+	"time"
 )
 
 type RconClient struct {
 	Addr     *net.UDPAddr
 	Password string
+
+	c *net.UDPConn
 }
+
+// https://wiki.zandronum.com/RCon_protocol
+
+// Messages that the server sends to the client always begin with one
+// of the following bytes:
+type SVRC byte
+const (
+	SVRC_OLDPROTOCOL SVRC = iota + 32
+	SVRC_BANNED
+	SVRC_SALT
+	SVRC_LOGGEDIN
+	SVRC_INVALIDPASSWORD
+	SVRC_MESSAGE
+	SVRC_UPDATE
+	SVRC_TABCOMPLETE
+	SVRC_TOOMANYTABCOMPLETES
+)
+
+// Messages that the client sends to the server always begin with one
+// of the following bytes:
+type CLRC byte
+const (
+	CLRC_BEGINCONNECTION CLRC = iota + 52
+	CLRC_PASSWORD
+	CLRC_COMMAND
+	CLRC_PONG
+	CLRC_DISCONNECT
+	CLRC_TABCOMPLETE
+)
+
+// Also, when the server sends SVRC_UPDATE, it's immediately followed
+// by another byte:
+type SVRCU byte
+const (
+	SVRCU_PLAYERDATA SVRCU = iota
+	SVRCU_ADMINCOUNT
+	SVRCU_MAP
+)
+
+const PROTOCOL_VERSION = 4
+const PONG_INTERVAL = time.Second * 5
 
 func NewRconClient(hostport, password string) (c *RconClient, err error) {
 	c = &RconClient{}
@@ -17,6 +62,47 @@ func NewRconClient(hostport, password string) (c *RconClient, err error) {
 		return
 	}
 
+	return
+}
+
+func (r *RconClient) ponger(t *time.Ticker) {
+	for range t.C {
+		_, err := r.c.Write([]byte{0xFF, byte(CLRC_PONG)})
+		if err != nil {
+			log.Printf("cannot pong: %s", err)
+		}
+	}
+}
+
+func (r *RconClient) Connect() (err error) {
+	r.c, err = net.DialUDP("udp", nil, r.Addr)
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		if err != nil && r.c != nil {
+			r.c.Close()
+			r.c = nil
+		}
+	} ()
+
+	_, err = r.c.Write([]byte{0xFF, byte(CLRC_BEGINCONNECTION), PROTOCOL_VERSION})
+	if err != nil {
+		return
+	}
+
+	go r.ponger(time.NewTicker(PONG_INTERVAL))
+
+	return
+}
+
+func (r *RconClient) readPacket() error {
+	return nil
+}
+
+func (r *RconClient) Close() (err error) {
+	_, err = r.c.Write([]byte{0xFF, byte(CLRC_DISCONNECT)})
 	return
 }
 
